0a8f93bb052253cbfe8abb1337f2686f68c73a94
rajithapgituser <96479822+rajithapgituser@users.noreply.github.com>
2022-05-10 15:54:28 +0530

    Update SpringbootBackendApplication.java

--- src/main/java/org/ibm/springboot/SpringbootBackendApplication.java
+++ src/main/java/org/ibm/springboot/SpringbootBackendApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
+
 @SpringBootApplication
 public class SpringbootBackendApplication {
 
@@ -24,12 +24,4 @@ public class SpringbootBackendApplication {
 			}
 		};
 	}
-	public class MyConfiguration implements WebMvcConfigurer {
-	    @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedMethods("*");
-	    }
-	}
-
 }
