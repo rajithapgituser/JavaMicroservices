1911552e3513fb86f8ae56b5d4ccbe8b3d16a77c
rajithapgituser <96479822+rajithapgituser@users.noreply.github.com>
2022-05-10 15:29:41 +0530

    Update SpringbootBackendApplication.java

--- src/main/java/org/ibm/springboot/SpringbootBackendApplication.java
+++ src/main/java/org/ibm/springboot/SpringbootBackendApplication.java
@@ -2,12 +2,25 @@ package org.ibm.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/employees").allowedOrigins("http://169.51.203.100:32404");
+			}
+		};
 	}
 
 }
